c5b54064367167d7d6defe0e50f243a34734018a
Vladimir <32133129+Voikgrib@users.noreply.github.com>
2019-03-04 17:28:29 +0300

    Add files via upload

--- script & cloud/record_server_edition.sh
+++ script & cloud/record_server_edition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LOGFILE=/home/voik/cams/script/dump.txt
+export LOGFILE=/home/voik/test/dump.txt
 
 echo " >>> Only local saves, cloud not implemented!" 	> $LOGFILE
 
@@ -21,21 +21,21 @@ CONF_FILE="$1"
 
 if [ -z $CONF_FILE ]
 then
-	echo "Usage: $0 <config_file>"									> $LOGFILE
-	echo ""															> $LOGFILE
-	echo "Config file format(name cameras.config):"					> $LOGFILE
-	echo "	segment_duration = 10 (in seconds)"						> $LOGFILE
-	echo "	days_for_segment_to_live = 10"							> $LOGFILE
-	echo "	camera_1_address = login:pass@127.0.0.1:554"			> $LOGFILE
-	echo "	..."													> $LOGFILE
-	echo "	camera_10_address = login10:pass10@127.0.0.10:554"		> $LOGFILE
+	echo "Usage: $0 <config_file>"									
+	echo ""															
+	echo "Config file format(name cameras.config):"					
+	echo "	segment_duration = 10 (in seconds)"						
+	echo "	days_for_segment_to_live = 10"							
+	echo "	camera_1_address = login:pass@127.0.0.1:554"			
+	echo "	..."													
+	echo "	camera_10_address = login10:pass10@127.0.0.10:554"		
 	exit
 fi
 
 test -e "$CONF_FILE"
 if [ $? -ne 0 ]
 then
-	echo "File '$CONF_FILE' nonexistent"							> $LOGFILE
+	echo "File '$CONF_FILE' nonexistent"							
 	exit
 fi
 
@@ -56,7 +56,7 @@ do
 	if [ -z ${!ADDRESS_VAR_NAME} ]
 	then
 		CAM_NUM=$(($i-1))
-		echo "$CAM_NUM cameras initialized."						> $LOGFILE
+		echo "$CAM_NUM cameras initialized."						
 		break
 	fi
 done
@@ -70,30 +70,32 @@ fi
 test -e "$temp_dir"
 if [ $? -ne 0 ]
 then
-	echo "Directory (temp)  '$temp_dir' nonexistent"				> $LOGFILE
+	echo "Directory (temp)  '$temp_dir' nonexistent"				
 	exit
 fi
 
 test -e "$cloud_dir"
 if [ $? -ne 0 ]
 then
-	echo "Directory (cloud) '$cloud_dir' nonexistent"				> $LOGFILE
+	echo "Directory (cloud) '$cloud_dir' nonexistent"				
 	exit
 fi
 
 # Print parsed config
 
-echo "	cloud_dir                = $cloud_dir"						> $LOGFILE
-echo "	temp_dir                 = $temp_dir"						> $LOGFILE
-echo "	segment_duration   	 	 = $segment_duration"				> $LOGFILE
-echo "	days_for_segment_to_live = $days_for_segment_to_live"		> $LOGFILE
+echo "	cloud_dir                = $cloud_dir"						
+echo "	temp_dir                 = $temp_dir"						
+echo "	segment_duration   	 	 = $segment_duration"				
+echo "	days_for_segment_to_live = $days_for_segment_to_live"		
 for i in $(seq $CAM_NUM)
 do
 	ADDRESS_VAR_NAME="camera_"$i"_address"
-	echo "	camera_${i}_address         = ${!ADDRESS_VAR_NAME}"		> $LOGFILE
+	echo "	camera_${i}_address         = ${!ADDRESS_VAR_NAME}"		
 	mkdir -p "$cloud_dir/camera_$i"
 	mkdir -p "$temp_dir/camera_$i"
 done
+
+echo
 
 prompt
 
@@ -118,22 +120,28 @@ do
 		-strftime 1 \
 		$temp_dir/camera_$i/record_%Y-%m-%d_%H-%M-%S.avi		> $LOGFILE
 
-	screen -list > $LOGFILE #debug
+	#screen -list #debug
 
-	echo "`date +\"[%D %H:%M:%S]\"` Cam #$i(${!ADDRESS_VAR_NAME}): screen return code: $?"
+	#echo "`date +\"[%D %H:%M:%S]\"` Cam #$i(${!ADDRESS_VAR_NAME}): screen return code: $?"
+	echo
 done
+
+# Regular sleep time
+SLEEP_TIME=$(($segment_duration))
+# First sleep time (need to start recording)
+FIRST_SLEEP_TIME=$(bc <<< "$segment_duration + 30") 
+
+sleep $FIRST_SLEEP_TIME
 
 while :
 do
-	SLEEP_TIME=$(($segment_duration))
-	echo "`date +\"[%D %H:%M:%S]\"` Sleep for $SLEEP_TIME"
+	#SLEEP_TIME=$(($segment_duration))
+	#echo "`date +\"[%D %H:%M:%S]\"` Sleep for $SLEEP_TIME" > $LOGFILE
 
 	screen -list > $LOGFILE #debug
 
 	# nado pofixsit prava dostupa
 	#find "$temp_dir/camera_$i" -type f -mmin +$segment_duration -exec mv -- '{}' $cloud_dir \;
-
-	sleep $SLEEP_TIME
 
 	for i in $(seq $CAM_NUM)
 	do
@@ -142,6 +150,35 @@ do
 		find "$temp_dir/camera_$i" -type f -mmin +$temp_min -exec mv -- '{}' "$cloud_dir/camera_$i" \;
 	done
 
+	sleep $SLEEP_TIME
+
+	# if one cam is broken, restart all
+	#if screen -ls | grep "(Detached)"
+	#then
+	killall screen
+	for i in $(seq $CAM_NUM)
+	do
+		ADDRESS_VAR_NAME="camera_"$i"_address"
+		echo "`date +\"[%D %H:%M:%S]\"` Cam #$i(${!ADDRESS_VAR_NAME}): Start recording" > $LOGFILE
+		screen -d -m \
+			-S record ffmpeg \
+			-rtsp_transport tcp \
+			-i rtsp://${!ADDRESS_VAR_NAME}/ch01.264?ptype=tcp \
+			-acodec copy \
+			-f segment \
+			-segment_time $segment_duration\
+			-segment_format avi \
+			-reset_timestamps 1 \
+			-copyts \
+			-flags global_header \
+			-strftime 1 \
+			$temp_dir/camera_$i/record_%Y-%m-%d_%H-%M-%S.avi		> $LOGFILE
+
+		screen -list > $LOGFILE #debug
+
+		echo "`date +\"[%D %H:%M:%S]\"` Cam #$i(${!ADDRESS_VAR_NAME}): screen return code: $?" > $LOGFILE
+	done			
+	#fi
 	echo
 done
 
